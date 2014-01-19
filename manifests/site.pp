183c8f0f4a0e90515cd73de5cbb0119f8849590a
briantwalter <walter.brian@gmail.com>
2014-01-18 18:32:03 -0800

    multi os

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,20 +1,25 @@
 node default {
 	#== files
 	file { '/tmp/puppet.ctl':
-		content => "this host is puppet mastered by ${servername}\n",
+		content => "this ${operatingsystem} host is puppet mastered by ${servername}\n",
 		ensure => present,
 	}
-	file { '/filer':
-		ensure => absent,
-		#ensure => link,
-		#target => '/home',
+
+	case $operatingsystem {
+	  'Ubuntu': {
+		file { '/filer':
+			ensure => absent,
+			#ensure => link,
+			#target => '/home',
+		}
+		#== includes
+		include motd
+		include ntpdate
+		include users
+		include services
+		include packages
+		include dotfiles
+		#include autoupdates
+	  }
 	}
-	#== includes
-	include motd
-	include ntpdate
-	include users
-	include services
-	include packages
-	include dotfiles
-	include autoupdates
 }
