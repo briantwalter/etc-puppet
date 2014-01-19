ea7b4a08cdb3d7ff4e6723bee2a2cf9da438ae07
briantwalter <walter.brian@gmail.com>
2014-01-18 18:48:49 -0800

    lint'ed site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,25 +1,28 @@
 node default {
-	#== files
-	file { '/tmp/puppet.ctl':
-		content => "this ${operatingsystem} host is puppet mastered by ${servername}\n",
-		ensure => present,
-	}
-
-	case $operatingsystem {
-	  'Ubuntu': {
-		file { '/filer':
-			ensure => absent,
-			#ensure => link,
-			#target => '/home',
-		}
-		#== includes
-		include motd
-		include ntpdate
-		include users
-		include services
-		include packages
-		include dotfiles
-		#include autoupdates
-	  }
-	}
+  #== files
+  file { '/tmp/puppet.ctl':
+    ensure  => present,
+    content => "this ${operatingsystem} host is puppet \
+      mastered by ${servername}\n"
+  }
+  #== case based on OS type
+  case $operatingsystem {
+    'Ubuntu': {
+      file { '/filer':
+      ensure => absent,
+      #ensure => link,
+      #target => '/home',
+      }
+      #== includes
+      include motd
+      include ntpdate
+      include users
+      include services
+      include packages
+      include dotfiles
+      include autoupdates
+    }
+    default: {
+    }
+  }
 }
